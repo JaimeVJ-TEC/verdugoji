75b666ec2e10946e07e9782205586d9920748d06
JaimeVJ-TEC <L19100265@nlaredo.tecnm.mx>
2022-03-29 14:21:57 -0500

    EventoGrid

---
+++ Parcial 2/EventoGrid/script.js
@@ -0,0 +1,10 @@
+window.onload = function()
+{
+    document.getElementById("grid1").addEventListener("click",function(e)
+    {
+        console.log(e.target.id)
+        
+        if(e.target.id !== "grid1")
+        document.getElementById(e.target.id).classList.toggle("cajaToggle");
+    });
+}
