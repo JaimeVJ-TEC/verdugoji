45719290a5076f8181d9f2b688046686d681cba0
JaimeVJ-TEC <L19100265@nlaredo.tecnm.mx>
2022-05-31 14:04:30 -0500

    New

---
+++ NEW/php/archivo.php
@@ -0,0 +1,12 @@
+<?php
+
+$registro['NumControl'] = '19100265';
+$registro['Nombre'] = 'Jose Pepe';
+$registro['Correo'] = 'jose_pepe@hotmail.com';
+$registro['Telefono'] = '8672546868';
+$registro['Movil'] = '8677104937';
+$registro['Direccion'] = '123 Revolucion';
+
+$registroJson = json_encode($registro);
+echo $registroJson;
+?>
